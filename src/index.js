dd66d414fbc386dea2d67d63b902b3c0f73c14d3
Zyrica <drevin@zyrica.com>
2022-12-06 00:15:06 +0100

    simplify flow
    Merge run-day into index
    Watch all days instead of each day

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
-import { execSync } from 'child_process';
-import { watch } from 'fs';
+import { readFileSync, watch } from 'fs';
 import { resolve } from 'path';
 import chalk from 'chalk';
+import { pathToFileURL } from 'url';
 
-const paths = {
-  days: resolve('src/2022'),
-  index: resolve('src/2022/Day 1/index.js'),
-};
+// Init
+const delay = 200;
+const timers = {};
 
-watch(paths.days, (_, name) => {
-  const path = resolve(paths.days, name);
-  console.clear();
-  console.log(chalk.cyan(name));
+watch(resolve('src/2022'), async (_, name) => {
+  clearTimeout(timers[name]);
+  timers[name] = setTimeout(() => runDay(name), delay);
+});
+
+console.clear();
+console.log('Watching all days for changes.');
+
+// Functions
+async function runDay(name) {
   try {
-    execSync(`node src/run-day.js "${path}"`, {stdio: 'inherit'});
+    console.clear();
+    console.log(chalk.cyan(name));
+
+    const path = resolve('src/2022/', name) + '/';
+    const {task1, task2} = await import(pathToFileURL(path + 'index.js'));
+    const tasks = [task1, task2];
+    const input = read(path + 'input');
+    const solution = read(path + 'solution');
+
+    tasks.forEach((task, i) => {
+      const result = task([...input]);
+      if (solution[i] === '' + result) {
+        console.log(result, chalk.green('Correct'));
+      } else {
+        console.log(result, chalk.red('Wrong'), solution[i]);
+      }
+    });
   } catch (e) {
     console.error(e);
   }
-});
+}
 
-console.log('Watching days for changes');
+function read(path) {
+  return readFileSync(path, 'utf-8').split('\r\n');
+}
