2313729218672f4afceb0e6da2361c128afe2059
Zyrica <drevin@zyrica.com>
2022-12-05 19:47:54 +0100

    Chalk
    Simplification
    Introduce chalk

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
 import { execSync } from 'child_process';
-import { readdirSync, watch } from 'fs';
-import { resolve, join } from 'path';
-
-
-const callOnceTimers = {};
-
-function callOnce(fn, delay) {
-  clearTimeout(callOnceTimers[fn]);
-  callOnceTimers[fn] = setTimeout(fn, delay);
-}
-
-const cyan = '\x1b[36m';
-const reset = '\x1b[0m';
+import { watch } from 'fs';
+import { resolve } from 'path';
+import chalk from 'chalk';
 
 const paths = {
-  days: resolve('src', '2022'),
+  days: resolve('src/2022'),
+  index: resolve('src/2022/Day 1/index.js'),
 };
 
-class Day {
-  constructor(name) {
-    this.name = name;
-    this.path = resolve(join(paths.days, name));
+watch(paths.days, (_, name) => {
+  const path = resolve(paths.days, name);
+  console.clear();
+  console.log(chalk.cyan(name));
+  try {
+    execSync(`node src/run-day.js "${path}"`, {stdio: 'inherit'});
+  } catch (e) {
+    console.error(e);
   }
-
-  watch() {
-    watch(this.path, () => callOnce(() => this.run(), 100));
-  }
-
-  run() {
-    console.log(cyan + this.name + reset);
-    try {
-      execSync(`node src/run-day.js "${this.path}"`, {stdio: 'inherit'});
-    } catch (e) {
-      console.error(e);
-    }
-  }
-}
-
-const days = readdirSync(paths.days).map(name => new Day(name));
-
-days.forEach(day => day.watch());
+});
 
 console.log('Watching days for changes');
--- src/run-day.js
+++ src/run-day.js
@@ -1,13 +1,10 @@
 import { join, resolve } from 'path';
 import { readFileSync } from 'fs';
 import { pathToFileURL } from 'url';
+import chalk from 'chalk';
 
-const red = '\x1b[31m';
-const green = '\x1b[32m';
-const reset = '\x1b[0m';
-
-const correct = green + 'Correct' + reset;
-const wrong = red + 'Wrong' + reset;
+const correct = chalk.green('Correct');
+const wrong = chalk.red('Wrong');
 
 const path = process.argv[2];
 const paths = {
@@ -22,5 +19,5 @@ const input = readFileSync(paths.input, 'utf-8').split('\r\n');
 const solution = readFileSync(paths.solution, 'utf-8').split('\r\n');
 
 const result = [task1([...input]), task2([...input])];
-console.log(result[0], solution[0] === '' + result[0] ? correct : wrong + `(${solution[0]})`);
-console.log(result[1], solution[1] === '' + result[1] ? correct : wrong + `(${solution[1]})`);
+console.log(result[0], solution[0] === '' + result[0] ? correct : wrong + ` - (${solution[0]})`);
+console.log(result[1], solution[1] === '' + result[1] ? correct : wrong + ` - (${solution[1]})`);
