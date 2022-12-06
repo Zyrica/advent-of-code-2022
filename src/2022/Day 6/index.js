301cbac2ce755e2a5102b9e1add0f0ceaaaca32e
Zyrica <drevin@zyrica.com>
2022-12-06 06:58:27 +0100

    Day 6

--- src/2022/Day 6/index.js
+++ src/2022/Day 6/index.js
@@ -1,14 +1,23 @@
-let a;
+let inputArray;
+
+function findFirstUniqueSlice(array, length) {
+  let marker;
+  for (let i = 0; i < array.length; i++) {
+    const set = new Set(array.slice(i, i + length));
+    if (set.size === length) {
+      marker = i + length;
+      break;
+    }
+  }
+  return marker;
+}
 
 export function task1(input) {
-  a = input[0].split('');
-  for (let i = 0; i < a.length; i++) {
-    if ((new Set(a.slice(i, i + 4))).size === 4) return i + 4;
-  }
+  inputArray = input[0].split('');
+
+  return findFirstUniqueSlice(inputArray, 4);
 }
 
 export function task2(input) {
-  for (let i = 0; i < a.length; i++) {
-    if ((new Set(a.slice(i, i + 14))).size === 14) return i + 14;
-  }
+  return findFirstUniqueSlice(inputArray, 14);
 }
